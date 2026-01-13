eea38cc2c2799fefca1161af96c753b76eafdba3
saivasanth26 <142992902+saivasanth26@users.noreply.github.com>
2026-01-13 18:25:35 +0530

    update

--- ASCII-script.sh
+++ ASCII-script.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, I am a DRAGON...RAAR" >dragon.txt
+cowsay -f dragon "Run for cover, I am a DRAGON...RAAR" >> dragon.txt
 cat dragon.txt
 ls -ltra
